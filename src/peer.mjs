655c14c592c46f4b697d6f33ba8150021ee03cd2
Maxime Lewandowski <lewandowski.mmjb@gmail.com>
2018-09-14 18:54:12 +0200

    peermjs: cleaning indentation

--- src/peer.mjs
+++ src/peer.mjs
@@ -3,134 +3,134 @@ import { Blockchain } from './blockchain'
 import { Block } from './blockchain.mjs';
 
 export class Peer {
-    constructor() {
-        this.blockchain = new Blockchain()
-        this.txStack = []
-        this.endpoint = new Endpoint()
-        this.endpoint.bci.getBlocks = async () => {
-            return this.blockchain.chain
+  constructor() {
+    this.blockchain = new Blockchain()
+      this.txStack = []
+      this.endpoint = new Endpoint()
+      this.endpoint.bci.getBlocks = async () => {
+        return this.blockchain.chain
+      }
+    this.endpoint.bci.setBlockchain = (blockchain) => {
+      this.blockchain.chain = blockchain
+    }
+    this.endpoint.bci.replaceBlockchain = (blockchain) => {
+      this.blockchain.replaceChain(blockchain)
+    }
+
+    this.endpoint.bci.checkBlock = (block) => {
+      return this.checkBlock(block)
+    }
+
+    this.endpoint.bci.addTx = async (tx) => {
+      console.log('transaction received')
+
+        if (process.env.MASTER) {
+          this.txStack.push(tx)
+            console.log('💸 transaction stacked', tx)
+        } else {
+          console.log('transfering to masterNode', tx)
+            this.endpoint.contactMasterNode(tx)
         }
-        this.endpoint.bci.setBlockchain = (blockchain) => {
-            this.blockchain.chain = blockchain
-        }
-        this.endpoint.bci.replaceBlockchain = (blockchain) => {
-            this.blockchain.replaceChain(blockchain)
-        }
-
-        this.endpoint.bci.checkBlock = (block) => {
-            return this.checkBlock(block)
-        }
-
-        this.endpoint.bci.addTx = async (tx) => {
-            console.log('transaction received')
-
-            if (process.env.MASTER) {
-                this.txStack.push(tx)
-                console.log('💸 transaction stacked', tx)
-            } else {
-                console.log('transfering to masterNode', tx)
-                this.endpoint.contactMasterNode(tx)
-            }
-        }
-        this.endpoint.start()
-        if (process.env.MASTER)
-            this.startMining()
     }
+    this.endpoint.start()
+      if (process.env.MASTER)
+        this.startMining()
+  }
 
-    computeBalances() {
-        let transactionList = {}
-        transactionList["root"] = 100
+  computeBalances() {
+    let transactionList = {}
+    transactionList["root"] = 100
 
-        this.blockchain.chain.forEach(block => {
-            block.data.forEach(tx => {
-                if (tx.valid === false)
-                    return
+      this.blockchain.chain.forEach(block => {
+          block.data.forEach(tx => {
+              if (tx.valid === false)
+              return
 
-                transactionList[tx.from] = transactionList[tx.from] || 0
-                transactionList[tx.to] = transactionList[tx.to] || 0
+              transactionList[tx.from] = transactionList[tx.from] || 0
+              transactionList[tx.to] = transactionList[tx.to] || 0
 
-                transactionList[tx.from] -= tx.amount
-                transactionList[tx.to] += tx.amount
-            })
-        })
+              transactionList[tx.from] -= tx.amount
+              transactionList[tx.to] += tx.amount
+              })
+          })
 
-        return transactionList
-    }
+    return transactionList
+  }
 
-    createBlock() {
-        let transactionList = this.computeBalances()
+  createBlock() {
+    let transactionList = this.computeBalances()
 
-        let resData = []
-        this.txStack.forEach(tx => {
-            transactionList[tx.to] = transactionList[tx.to] || 0
-            transactionList[tx.from] = transactionList[tx.from] || 0
-            if (transactionList[tx.from] - tx.amount >= 0) {
-                resData.push({ ...tx, ok: true })
-                transactionList[tx.to] += tx.amount
-                transactionList[tx.from] -= tx.amount
-            }
-            else
-                resData.push({ ...tx, ok: false })
-        })
-        return new Block(this.blockchain.chain.length,
-            resData,
-            this.blockchain.lastBlock().hash,
-            Date.now());
-    }
+      let resData = []
+      this.txStack.forEach(tx => {
+          transactionList[tx.to] = transactionList[tx.to] || 0
+          transactionList[tx.from] = transactionList[tx.from] || 0
+          if (transactionList[tx.from] - tx.amount >= 0) {
+          resData.push({ ...tx, ok: true })
+          transactionList[tx.to] += tx.amount
+          transactionList[tx.from] -= tx.amount
+          }
+          else
+          resData.push({ ...tx, ok: false })
+          })
+    return new Block(this.blockchain.chain.length,
+        resData,
+        this.blockchain.lastBlock().hash,
+        Date.now());
+  }
 
-    checkBlock(blockToCheck) {
-        let transactionList = this.computeBalances()
-        let isValid = true
+  checkBlock(blockToCheck) {
+    let transactionList = this.computeBalances()
+      let isValid = true
 
-        // verify hash of previous block
+      // verify hash of previous block
 
-        blockToCheck.data.forEach(tx => {
-            if (tx.valid === false ) {
-                if (transactionList[tx.from] - tx.amount >= 0)
-                    isValid = false
-                return
-            }
+      blockToCheck.data.forEach(tx => {
+          if (tx.valid === false ) {
+          if (transactionList[tx.from] - tx.amount >= 0)
+          isValid = false
+          return
+          }
 
-            if (transactionList[tx.from] - tx.amount < 0) {
-                isValid = false
-                return
-            }
+          if (transactionList[tx.from] - tx.amount < 0) {
+          isValid = false
+          return
+          }
 
-            transactionList[tx.from] = transactionList[tx.from] || 0
-            transactionList[tx.to] = transactionList[tx.to] || 0
+          transactionList[tx.from] = transactionList[tx.from] || 0
+          transactionList[tx.to] = transactionList[tx.to] || 0
 
-            transactionList[tx.from] -= tx.amount
-            transactionList[tx.to] += tx.amount
-        })
+          transactionList[tx.from] -= tx.amount
+          transactionList[tx.to] += tx.amount
+          })
 
-        return isValid
+    return isValid
 
-    }
+  }
 
 
-    async mine() {
-        const block = this.createBlock()
-        this.txStack = []
+  async mine() {
+    const block = this.createBlock()
+      this.txStack = []
 
-        console.log('Verifying block...')
-        const { votes, peers } = await this.endpoint.broadcastBlock(block)
-        console.log(votes)
+      console.log('Verifying block...')
+      const { votes, peers } = await this.endpoint.broadcastBlock(block)
+      console.log(votes)
 
-        let trueCount = votes.reduce((a, b) => a + b, 0) + 1
+      let trueCount = votes.reduce((a, b) => a + b, 0) + 1
 
-        if (trueCount >= Math.floor((2 / 3) * peers + 1)) {
-            console.log('⛏️  Block verified', block.data)
-            this.blockchain.chain.push(block)
-            // broadcast blockchain
-        } else
-            console.log('❌ Block invalid', block.data)
-    }
+      if (trueCount >= Math.floor((2 / 3) * peers + 1)) {
+        console.log('⛏️  Block verified', block.data)
+          this.blockchain.chain.push(block)
+          // broadcast blockchain
+      } else
+        console.log('❌ Block invalid', block.data)
+  }
 
-    startMining() {
-        this.minerPid = setInterval(() => this.mine(), 3000)
-    }
+  startMining() {
+    this.minerPid = setInterval(() => this.mine(), 3000)
+  }
 
-    stopMining() {
-        clearInterval(this.minerPid)
-    }
+  stopMining() {
+    clearInterval(this.minerPid)
+  }
 }
