41457e06ea7466b4ac463303f078a9b108166b7e
Maxime Lewandowski <lewandowski.mmjb@gmail.com>
2018-09-14 06:36:32 +0200

    endpoint: addtx & getBlocks

--- src/endpoint.mjs
+++ src/endpoint.mjs
@@ -3,6 +3,7 @@ import Router from 'koa-router'
 import logger from 'koa-logger'
 import bodyParser from 'koa-bodyparser'
 import socketify from 'koa-websocket'
+import json from 'koa-json'
 
 class Endpoint {
   constructor() {
@@ -12,13 +13,20 @@ class Endpoint {
     this.httpRouter = new Router()
     this.wsRouter = new Router()
 
+
+    // Blockchain getter interface
+    this.bci = {
+      getBlocks: null,
+      addTx: null
+    }
+
     this.httpRouter
-      .all('/', this.helloworld)
-      .get('/blocks', this.getBlocks)
-      .get('/peers', this.getPeers)
+      .get('/blocks', async ctx => await this.getBlocks(ctx))
+      .post('/tx', async ctx => await this.addTx(ctx))
 
     this.app
       .use(logger())
+      .use(json())
       .use(bodyParser())
       .use(this.httpRouter.routes())
 
@@ -39,12 +47,41 @@ class Endpoint {
 
 
   // Routes handlers
-  async helloworld(ctx, next) {
-    ctx.body = 'Hello World'
+  async getBlocks(ctx) {
+    if (this.bci.getBlocks)
+      ctx.body = await this.bci.getBlocks()
+    else
+    {
+      ctx.status = 500
+      ctx.body = { error: 'no blockchain connected' }
+    }
   }
-  async getBlocks(ctx, next) {
-    ctx.body = 
+
+  async addTx(ctx) {
+    console.dir(this, {depth: 1, color: true})
+    if (this.bci.addTx) {
+      // Check missing props
+      const txProps = ['from', 'to', 'amount', 'msg']
+      const missingProps = txProps.filter(prop => !(prop in ctx.req.body))
+      if (missingProps.length > 0) {
+        ctx.status = 400
+        ctx.body = { error: `mandatory parameter '${prop}' is missing` }
+      } else {
+
+        const cleanedTx = txProps
+          .reduce((tx, prop) => ({ ...tx, [prop]: ctx.req.body[prop] }), {})
+
+        await this.bci.addTx(cleanedTx)
+        ctx.status = 200
+      }
+    }
+    else
+    {
+      ctx.status = 500
+      ctx.body = { error: 'no blockchain connected' }
+    }
   }
+
   async getPeers(ctx, next) {
     ctx.body = 'Hello World'
   }
