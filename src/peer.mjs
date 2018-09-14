b74e45ffd231d172a421876058e5159834d4b292
Cyril Forveille <cyril.forveille@epita.fr>
2018-09-14 16:58:49 +0700

    nothing important

--- src/peer.mjs
+++ src/peer.mjs
@@ -78,6 +78,9 @@ export class Peer {
     checkBlock(blockToCheck) {
         let transactionList = this.computeBalances()
         let isValid = true
+
+        // verify hash of previous block
+
         blockToCheck.data.forEach(tx => {
             if (tx.valid === false ) {
                 if (transactionList[tx.from] - tx.amount >= 0)
